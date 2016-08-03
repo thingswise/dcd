8c946ffd2c525057b41ec4b77350b1f632340c87
Alexander Lukichev <alexander.s.lukichev@gmail.com>
2016-08-03 05:55:06 -0500

    use atomic operation to replace file content; avoid using cassandra batches

--- storage.go
+++ storage.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+  "strconv"
+  "time"
 	"github.com/gocql/gocql"
 )
 
@@ -14,8 +16,8 @@ func (s *Storage) initStorage() error {
 	if err := s.Session.Query("CREATE KEYSPACE IF NOT EXISTS dconf WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 3 };").Exec(); err != nil {
 		return err
 	}
-	if err := s.Session.Query(`CREATE TABLE IF NOT EXISTS dconf.files ( 
-          entryname text,
+	if err := s.Session.Query(`CREATE TABLE IF NOT EXISTS dconf.files (
+      entryname text,
 		  block     int,
 		  data      blob,
 		  hash      text,
@@ -26,33 +28,81 @@ func (s *Storage) initStorage() error {
 }
 
 func (s *Storage) getHashes() ([]string, error) {
-	iter := s.Session.Query("SELECT block, hash FROM dconf.files WHERE entryname=?;", s.File).Iter()
-	res := make([]string, 0)
+  var hash string
+  res := make([]string, 0)
 	var block int
-	var hash string
+
+  // v2: files[entryname=?,block=-1].hash points to a reference to a hash list
+  iter_v2 := s.Session.Query("SELECT hash FROM dconf.files WHERE entryname=? AND block=?;", s.File, -1).Iter()
+  for iter_v2.Scan(&hash) {
+    if err := iter_v2.Close(); err != nil {
+      return nil, err
+    }
+
+    iter_v2_1 := s.Session.Query("SELECT block, hash FROM dconf.files WHERE entryname=?;", hash).PageSize(256).Iter()
+    for iter_v2_1.Scan(&block, &hash) {
+  		res = set(res, block, hash)
+  	}
+    if err := iter_v2_1.Close(); err != nil {
+      return nil, err
+    }
+
+  	return res, nil
+  }
+
+  // v1: don't use indirect addressing of hash lists
+	iter := s.Session.Query("SELECT block, hash FROM dconf.files WHERE entryname=?;", s.File).PageSize(256).Iter()
+
 	for iter.Scan(&block, &hash) {
 		res = set(res, block, hash)
 	}
+  if err := iter.Close(); err != nil {
+    return nil, err
+  }
 	return res, nil
 }
 
 func (s *Storage) setHashes(oldHashes, hashes []string) error {
-	batch := s.Session.NewBatch(gocql.LoggedBatch)
+  now := time.Now()
+  new_ref := s.File+":*"+strconv.FormatInt(now.Unix(), 10)
+
+  var old_ref string
+  var old_version bool = true
+
+  iter_v2 := s.Session.Query("SELECT hash FROM dconf.files WHERE entryname=? AND block=?;", s.File, -1).Iter()
+  for iter_v2.Scan(&old_ref) {
+    old_version = false
+  }
+  if err := iter_v2.Close(); err != nil {
+    return err
+  }
 
 	newHashes := make(map[string]bool)
 
 	for i := 0; i < len(hashes); i++ {
-		batch.Query("INSERT INTO dconf.files(entryname, block, data, hash) VALUES (?,?,?,?);", s.File, i, make([]byte, 0), hashes[i])
+		if err := s.Session.Query("INSERT INTO dconf.files(entryname, block, data, hash) VALUES (?,?,?,?);",
+      new_ref, i, make([]byte, 0), hashes[i]).Exec(); err != nil {
+      orig_err := err
+      s.Session.Query("DELETE FROM dconf.files WHERE entryname=?;", new_ref).Exec()
+      return orig_err
+    }
 		newHashes[hashes[i]] = true
 	}
 
-	for i := len(hashes); i < len(oldHashes); i++ {
-		batch.Query("DELETE FROM dconf.files WHERE entryname=? AND block=?;", s.File, i)
-	}
+  if err := s.Session.Query("INSERT INTO dconf.files(entryname, block, data, hash) VALUES (?,?,?,?);",
+     s.File, -1, make([]byte, 0), new_ref).Exec(); err != nil {
+    orig_err := err
+    s.Session.Query("DELETE FROM dconf.files WHERE entryname=?;", new_ref).Exec()
+    return orig_err
+  }
 
-	if err := s.Session.ExecuteBatch(batch); err != nil {
-		return err
-	}
+  if old_version {
+	  for i := len(hashes); i < len(oldHashes); i++ {
+		  s.Session.Query("DELETE FROM dconf.files WHERE entryname=? AND block=?;", s.File, i).Exec()
+	  }
+  } else {
+    s.Session.Query("DELETE FROM dconf.files WHERE entryname=?;", old_ref).Exec()
+  }
 
 	for _, h := range oldHashes {
 		if _, ok := newHashes[h]; !ok {
@@ -76,10 +126,12 @@ func (s *Storage) setHashes(oldHashes, hashes []string) error {
 func (s *Storage) readChunk(h string) ([]byte, error) {
 	log.Debug("Storage:readChunk(%s)", h)
 	iter := s.Session.Query("SELECT data FROM dconf.files WHERE entryname=? AND block=0;", s.File+":"+h).Iter()
+  defer iter.Close()
 	var data []byte
 	for iter.Scan(&data) {
 		return data, nil
 	}
+
 	return nil, fmt.Errorf("File %s: Chunk %s not found", s.File, h)
 }
 
